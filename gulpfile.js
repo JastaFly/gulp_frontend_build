da5afce046c72b810a9b617ed8dddb028e05b0ec
Владислав В. Мелешкин <vmeleshkin@local.koround.ru>
2026-02-02 12:09:42 +0300

    Refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-'use strict';
-const gulp = require('gulp');
-const {series, parallel, src, dest, watch} = require('gulp');
-const browserSync = require('browser-sync');
-const reload = browserSync.reload;
-const prefixer = require('gulp-autoprefixer');
-const rigger = require('gulp-rigger');
-const rename = require("gulp-rename");
-const less = require('gulp-less');
-const cssmin = require('gulp-cssmin');
+'use strict'
 
+const {parallel, src, dest, watch} = require('gulp')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+const prefixer = require('gulp-autoprefixer')
+const rigger = require('gulp-rigger')
+const rename = require('gulp-rename')
+const less = require('gulp-less')
+const cssmin = require('gulp-cssmin')
+const terser = require('gulp-terser')
 
 function html() {
     return src('./src/index.html')
         .pipe(rigger())
-        .pipe(dest('./build/'))
+        .pipe(dest('.'))
         .pipe(reload({stream: true}))
 }
 
@@ -21,72 +21,59 @@ function less_translator() {
     return src('./src/less/main.less')
         .pipe(less())
         .pipe(cssmin())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(prefixer({
-            cascade: false
-        }))
-        .pipe(dest('./build/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(prefixer({cascade: false}))
+        .pipe(dest('./css'))
         .pipe(reload({stream: true}))
 }
 
-function img() {
-    return src('./src/img/**/*')
-        .pipe(dest('./build/img'))
-}
-
 function fonts() {
-    return src('./src/fonts/**/*')
-        .pipe(dest('./build/fonts'))
+    return src('./src/fonts/**/*').pipe(dest('./fonts'))
 }
 
 function libs() {
-    return src('./src/libs/**/*')
-        .pipe(dest('./build/libs'))
+    return src('./src/libs/**/*').pipe(dest('./libs'))
 }
 
 function js() {
     return src('./src/js/**/*')
-        .pipe(dest('./build/js'))
+        .pipe(terser({mangle: true, compress: true, keep_fnames: false}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('./js'))
         .pipe(reload({stream: true}))
 }
+
 function php() {
-    return src('./src/php/**/*')
-        .pipe(dest('./build/php'))
+    return src('./src/php/**/*').pipe(dest('./php'))
 }
 
-
 const config = {
-    server: {
-        baseDir: "./build"
-    },
+    server: {baseDir: '.'},
     tunnel: false,
     host: 'localhost',
     port: 9000,
-    logPrefix: "jf"
-};
+    logPrefix: 'jf'
+}
 
 function webServer(cb) {
-    browserSync(config);
-    cb();
+    browserSync(config)
+    cb()
 }
+
 function sound() {
-    return src('./src/sound/**/*')
-        .pipe(dest('./build/sound'))
+    return src('./src/sound/**/*').pipe(dest('./sound'))
 }
 
 function watchAll(cb) {
-    watch('./src/*.html', html);
-    watch('./src/blocks/*.html', html);
-    watch('./src/less/*.less', less_translator);
-    watch('./src/img/**/*', img);
-    watch('./src/fonts/**/*', fonts);
-    watch('./src/libs/**/*', libs);
-    watch('./src/js/**/*', js);
-    watch('./src/php/**/*', php);
-    watch('./src/sound/**/*', sound);
-    cb();
+    watch('./src/*.html', html)
+    watch('./src/blocks/*.html', html)
+    watch('./src/less/*.less', less_translator)
+    watch('./src/fonts/**/*', fonts)
+    watch('./src/libs/**/*', libs)
+    watch('./src/js/**/*', js)
+    watch('./src/php/**/*', php)
+    watch('./src/sound/**/*', sound)
+    cb()
 }
 
-exports.default = parallel(webServer, watchAll);
+exports.default = parallel(webServer, watchAll)
